a6ab858c97c065be73ec28966ab1e8c09b44a4c0
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-07-30 11:35:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -135,3 +135,26 @@ async function initInnerTube() {
 process.on('unhandledRejection', console.error);
 
 initInnerTube();
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
+
+server.listen({
+  port: PORT,
+});

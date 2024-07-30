2d818906b68f3dec4ffd1cd64869a69b8720bc93
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-07-30 11:42:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,68 @@
-"use strict";
 import express from 'express';
 import http from 'node:http';
+import { createBareServer } from "@tomphttp/bare-server-node";
 import path from 'node:path';
 import cors from 'cors';
 
 const __dirname = process.cwd();
 const server = http.createServer();
 const app = express(server);
-const PORT = process.env.PORT || 3000;
+const bareServer = createBareServer('/bare/');
+const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.public(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/ypxa/y/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
+});
+
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
+});
+
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error.ejs'));
 });
 
 server.on('request', (req, res) => {
-  app(req, res);
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
+
+server.listen({
+  port: PORT,
 });
